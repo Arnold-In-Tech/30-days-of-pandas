f48640e2dca62cb99b10ce52cb03ef065bbbaa45
Arnold-In-Tech <g16a7782@gmail.com>
2024-09-06 10:57:34 +0300

    department highest sal

--- data_manipulation.py
+++ data_manipulation.py
@@ -140,16 +140,101 @@ def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     return result
 
 
+
+
+# Q 11
+
+# Write a solution to find employees who have the highest salary in each of the departments.
+
+# Return the result table in any order.
+
+# The result format is in the following example.
+
+ 
+
+# Example 1:
+
+# Input: 
+# Employee table:
+# +----+-------+--------+--------------+
+# | id | name  | salary | departmentId |
+# +----+-------+--------+--------------+
+# | 1  | Joe   | 70000  | 1            |
+# | 2  | Jim   | 90000  | 1            |
+# | 3  | Henry | 80000  | 2            |
+# | 4  | Sam   | 60000  | 2            |
+# | 5  | Max   | 90000  | 1            |
+# +----+-------+--------+--------------+
+# Department table:
+# +----+-------+
+# | id | name  |
+# +----+-------+
+# | 1  | IT    |
+# | 2  | Sales |
+# +----+-------+
+# Output: 
+# +------------+----------+--------+
+# | Department | Employee | Salary |
+# +------------+----------+--------+
+# | IT         | Jim      | 90000  |
+# | Sales      | Henry    | 80000  |
+# | IT         | Max      | 90000  |
+# +------------+----------+--------+
+# Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    
+    # widen the maximum per group with transform, check equality against it to detect maximums and index with that mask
+    employee_largest_df = employee.loc[employee["salary"].eq(employee.groupby("departmentId")["salary"].transform("max"))]
+
+    # select specific columns and rename departmentId to id
+    employee_largest_df = employee_largest_df[['name' ,'salary', 'departmentId']].rename(columns = {'departmentId':'id'})
+
+    # merge df1 to department df. rename columns, and drop id column
+    return pd.merge(employee_largest_df, department, on='id', how='inner').rename(columns = {'name_x':'Employee', 'name_y':'Department', 'salary': 'Salary'}).drop('id', axis=1)
+
+
+# ref: https://stackoverflow.com/questions/75226607/get-all-the-rows-with-max-value-in-a-pandas-dataframe-column-in-python
+# The transform() method allows you to execute a function for each value of the DataFrame.
+
+
+# Alternative
+
+import pandas as pd
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    
+    df = employee.merge(department, left_on ='departmentId', right_on = 'id', how ='inner')
+
+    df['max_salary'] = df.groupby('name_y') ['salary'].transform(max)
+
+    return df[df.salary == df.max_salary]   [['name_y','name_x','max_salary']].rename(columns = {'name_y': 'Department', 'name_x':'Employee', 'max_salary': 'Salary'})
+ 
+
+
+
+
+
 # === test code
 
-# df = pd.read_csv('./test_data.csv', sep=',')
-# print(nth_highest_salary(df, 2))
+# employee = pd.read_csv('./test_data.csv', sep=',')
+# department = pd.read_csv('./department.csv', sep=',')
+
+# print(department_highest_salary(employee, department))
+
+
+
+
+
 
 
 # =============================================================== #
 
-
-
 # N/B 
 # . drop_duplicates() will keep the first instance of a duplicate 
 # row and remove any others.
+
+# Q 11. Using .apply to get multiple rows with the highets value
+# Use groupby to group data by 'departmentId' and apply a lambda function to get employees with highest salary in each group
+    # highest_salary_df = merged_df.groupby('departmentId').apply(lambda x: x[x['salary'] == x['salary'].max()])
